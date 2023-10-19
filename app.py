98fb7ecb387c290079b8492b378e870eac55f436
Joakim Persson <joape526@student.liu.se>
2023-10-20 00:33:15 +0200

    added instructions to temp data list

--- app.py
+++ app.py
@@ -20,6 +20,12 @@ recipe_list = [
             "Basil",
             "Parmesan Cheese",
         ],
+        "instructions": [
+            "Boil pasta until al dente.",
+            "In a saucepan, heat olive oil and sauté minced garlic.",
+            "Add basil and tomato sauce to the pan.",
+            "Serve sauce over cooked pasta and garnish with grated Parmesan cheese."
+        ],
     },
     {
         "id": 2,
@@ -35,6 +41,11 @@ recipe_list = [
             "Bell Peppers",
             "Onions",
         ],
+        "instructions": [
+            "Roll out pizza dough and spread tomato sauce over it.",
+            "Add mozzarella cheese, pepperoni, bell peppers, and onions as toppings.",
+            "Bake in the oven until the crust is golden and the cheese is melted and bubbly."
+        ],
     },
     {
         "id": 3,
@@ -49,6 +60,11 @@ recipe_list = [
             "Lettuce",
             "Tomatoes",
             "Sour Cream",
+        ],
+        "instructions": [
+            "Cook ground beef in a pan, seasoning it with taco seasoning.",
+            "Warm tortillas in a dry skillet or microwave.",
+            "Assemble tacos with cooked beef, lettuce, tomatoes, and sour cream."
         ],
     },
     {
@@ -66,6 +82,11 @@ recipe_list = [
             "Ketchup",
             "Mustard",
         ],
+        "instructions": [
+            "Shape ground beef into patties and cook on a grill or stovetop.",
+            "Toast burger buns on the grill.",
+            "Assemble burgers with lettuce, tomatoes, cheese, ketchup, and mustard."
+        ],
     },
     {
         "id": 5,
@@ -74,6 +95,12 @@ recipe_list = [
         "description": "Savor the flavors of fresh sushi rolls.",
         "foodType": "Japanese",
         "ingredients": ["Sushi Rice", "Nori Seaweed", "Fish", "Cucumber", "Avocado"],
+        "instructions": [
+            "Prepare sushi rice and season it with rice vinegar and sugar.",
+            "Place a sheet of nori seaweed on a bamboo sushi rolling mat.",
+            "Spread rice on the nori, add fish, cucumber, and avocado.",
+            "Roll the sushi and cut it into bite-sized pieces."
+        ],
     },
     {
         "id": 6,
@@ -88,6 +115,12 @@ recipe_list = [
             "Mozzarella Cheese",
             "Tomato Sauce",
         ],
+        "instructions": [
+            "Cook lasagna noodles until al dente.",
+            "Brown ground beef in a skillet and mix with tomato sauce.",
+            "Layer lasagna noodles with meat sauce, ricotta, and mozzarella cheese.",
+            "Bake until cheese is bubbly and golden."
+        ],
     },
     {
         "id": 7,
@@ -96,6 +129,11 @@ recipe_list = [
         "description": "A spicy and aromatic Thai dish with vegetables and chicken.",
         "foodType": "Thai",
         "ingredients": ["Chicken", "Coconut Milk", "Green Curry Paste", "Vegetables"],
+        "instructions": [
+            "Cook chicken in a pan until browned.",
+            "Add coconut milk and green curry paste.",
+            "Simmer with vegetables until cooked through."
+        ],
     },
     {
         "id": 8,
@@ -104,6 +142,11 @@ recipe_list = [
         "description": "A refreshing salad with olives, feta cheese, and fresh vegetables.",
         "foodType": "Mediterranean",
         "ingredients": ["Lettuce", "Olives", "Feta Cheese", "Cucumbers", "Tomatoes"],
+        "instructions": [
+            "Chop lettuce, olives, and cucumbers.",
+            "Crumble feta cheese over the salad.",
+            "Add diced tomatoes and drizzle with olive oil."
+        ],
     },
     {
         "id": 9,
@@ -119,6 +162,11 @@ recipe_list = [
             "Butter",
             "Chocolate",
         ],
+        "instructions": [
+            "Mix dry ingredients and wet ingredients separately.",
+            "Combine and bake until a toothpick comes out clean.",
+            "Frost the cake with melted chocolate."
+        ],
     },
     {
         "id": 10,
@@ -132,6 +180,11 @@ recipe_list = [
             "Cream",
             "Parmesan Cheese",
             "Garlic",
+        ],
+        "instructions": [
+            "Cook pasta until al dente.",
+            "Sauté chicken and garlic, then add cream and Parmesan cheese.",
+            "Toss cooked pasta with the creamy sauce."
         ],
     },
     {
@@ -147,6 +200,12 @@ recipe_list = [
             "Mozzarella Cheese",
             "Tomato Sauce",
         ],
+        "instructions": [
+            "Cook lasagna noodles until al dente.",
+            "Brown ground beef in a skillet and mix with tomato sauce.",
+            "Layer lasagna noodles with meat sauce, ricotta, and mozzarella cheese.",
+            "Bake until cheese is bubbly and golden."
+        ],
     },
     {
         "id": 12,
@@ -155,6 +214,11 @@ recipe_list = [
         "description": "A spicy and aromatic Thai dish with vegetables and chicken.",
         "foodType": "Thai",
         "ingredients": ["Chicken", "Coconut Milk", "Green Curry Paste", "Vegetables"],
+        "instructions": [
+            "Cook chicken in a pan until browned.",
+            "Add coconut milk and green curry paste.",
+            "Simmer with vegetables until cooked through."
+        ],
     },
     {
         "id": 13,
@@ -163,6 +227,11 @@ recipe_list = [
         "description": "A refreshing salad with olives, feta cheese, and fresh vegetables.",
         "foodType": "Mediterranean",
         "ingredients": ["Lettuce", "Olives", "Feta Cheese", "Cucumbers", "Tomatoes"],
+        "instructions": [
+            "Chop lettuce, olives, and cucumbers.",
+            "Crumble feta cheese over the salad.",
+            "Add diced tomatoes and drizzle with olive oil."
+        ],
     },
     {
         "id": 14,
@@ -178,6 +247,11 @@ recipe_list = [
             "Butter",
             "Chocolate",
         ],
+        "instructions": [
+            "Mix dry ingredients and wet ingredients separately.",
+            "Combine and bake until a toothpick comes out clean.",
+            "Frost the cake with melted chocolate."
+        ],
     },
     {
         "id": 15,
@@ -191,6 +265,11 @@ recipe_list = [
             "Cream",
             "Parmesan Cheese",
             "Garlic",
+        ],
+        "instructions": [
+            "Cook pasta until al dente.",
+            "Sauté chicken and garlic, then add cream and Parmesan cheese.",
+            "Toss cooked pasta with the creamy sauce."
         ],
     },
 ]
